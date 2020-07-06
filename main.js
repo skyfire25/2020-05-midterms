c8353c5ac6dc0a49c39d5df1c2327ca9d12f4a41
Carlos L Molina <carlos.molina@codeimmersives.com>
2020-07-06 12:56:05 -0400

    changes

--- main.js
+++ main.js
@@ -1,3 +1,56 @@
+function capitalizeSentences(str){
+let everySen = ''
+for(let i =0; i< everySen.length; i++){
+if(everySen.length[i] < everySen.length){
+  everySen[i].charAt().toUpperCase();
+}
+let str= everySen.join('')
+}
+return str
+}
+
+function isValidPassword (str){
+for(let i = 0; i < str.length; i++){
+  if(str[i] === str.length){
+  }else if(str[i] === str[i] + ''){
+return false
+}else if(str[i] === str.length < 12){
+  return false
+}else{
+return true
+}
+}
+}
+
+function makeHalfSquares(num1){
+  let halve = []
+  for(let i= 0; i < num1.length; i++){
+if (halve[i] === num1.length){
+  }else if(){
+
+  }
+
+  }
+
+
+    function countAs(grade){
+for(let i = 0; i < grade.length; i++){
+
+}
+    }
+
+  }
+    function deleteMiddleLetters(){
+
+    }
+
+    function lastIndexOfSpace(){
+
+    }
+
+    function hyphenateName(){
+
+    }
 
 
 if (typeof capitalizeSentences === 'undefined') {
